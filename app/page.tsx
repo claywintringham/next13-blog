062db50a0fe3ced023db4d862618e6cd73ce687e
Clay Wintringham <aikenwintringham@outlook.com>
2023-06-19 11:30:33 +0800

    delete profile photo

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='px-6 mx-auto'>
-      <p className='mt-12 mb-12 text-3xl text-center dark:text-white'>Hello and Welcome!
+      <p className='mt-12 mb-12 text-3xl text-center dark:text-white'>
       <span className='whitespace-nowrap'>
-        I am <span className='font-bold'>Clay</span>
+        I am <span className='font-bold'>Clay</span>!
       </span>
       </p>
       <Posts />
